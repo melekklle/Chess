13949d39aabf4919a4f2d4f1ec4c5e93ce088c3a
Melek <rukiyemelek12@gmail.com>
2025-08-02 00:35:55 +0300

    update

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,43 @@
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity, Alert, View, Image } from 'react-native';
 
 export default function ProfileScreen() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <Text style={styles.profile}>Profil</Text>
+
+        <View style={styles.profilestyle}>
+          <TouchableOpacity onPress={() => Alert.alert('Profil', 'Mel\'in profiline tıkladınız')}>
+            <View style={styles.profileRow}>
+              <Image 
+                source={require("../image/mel.jpeg")}
+                style={styles.profileImage}
+              />
+
+              <View style={styles.textContainer}>
+                <Text style={styles.profileName}>Melek</Text>
+                <Text style={styles.profileEmail}>mel@example.com</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.profilestyle}>
+                  <TouchableOpacity onPress={() => Alert.alert('Profil', 'Mel\'in profiline tıkladınız')}>
+                    <View style={styles.profileRow}>
+                      <Image 
+                        source={require("../image/mel.jpeg")}
+                        style={styles.profileImage}
+                      />
+
+                      <View style={styles.textContainer}>
+                        <Text style={styles.profileName}>Melek</Text>
+                        <Text style={styles.profileEmail}>mel@example.com</Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+
       </ScrollView>
     </SafeAreaView>
   );
@@ -22,6 +54,37 @@ const styles = StyleSheet.create({
   profile: {
     fontSize: 20,
     fontWeight: 'bold',
-    alignSelf: 'center', 
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  profilestyle: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#769656',
+    borderRadius: 10,
+    padding: 10,
+     marginBottom: 15,
+      width: '70%',
+  },
+  profileRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15,
+  },
+  textContainer: {
+    justifyContent: 'center',
+  },
+  profileName: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'white',
+  },
+  profileEmail: {
+    fontSize: 14,
+    color: 'white',
   },
 });
